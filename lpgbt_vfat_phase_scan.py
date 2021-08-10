5f1506dada776d1786043fc58ac7483d49738c3e
Abhisek Datta <abhisek.datta@cern.ch>
2021-08-09 21:24:20 -0700

    minor updates

--- lpgbt_vfat_phase_scan.py
+++ lpgbt_vfat_phase_scan.py
@@ -223,7 +223,7 @@ def find_phase_center(err_list):
     # cover the case when there are no edges, just pick the center
     if (ngood==len(err_list_doubled)):
         ngood_max  = int(ngood/2)
-        ngood_edge =len(err_list_doubled)-1
+        ngood_edge = len(err_list_doubled)-1
 
     if (ngood_max>0):
         ngood_width = ngood_max
@@ -231,9 +231,9 @@ def find_phase_center(err_list):
         if (ngood_max % 2 == 0):
             ngood_center = ngood_edge - int(ngood_max/2) -1
             if (err_list_doubled[ngood_edge] > err_list_doubled[ngood_edge-ngood_max-1]):
-                ngood_center=ngood_center
+                ngood_center = ngood_center
             else:
-                ngood_center=ngood_center+1
+                ngood_center = ngood_center+1
         # odd windows
         else:
             ngood_center = ngood_edge - int(ngood_max/2) - 1;
