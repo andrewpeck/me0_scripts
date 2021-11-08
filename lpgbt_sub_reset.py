d56a73235d84b0a9235dec843242849ea246c1e5
Antonett <presurvivor@gmail.com>
2021-11-08 13:45:10 -0800

    update

--- lpgbt_sub_reset.py
+++ lpgbt_sub_reset.py
@@ -12,8 +12,7 @@ def convert_gpio_reg(gpio):
     reg_data |= (0x01 << bit)
     return reg_data
 
-def lpgbt_sub_reset(system, oh_select, gbtid_list):
-    print("SUB RESET\n")
+def lpgbt_sub_reset(system, oh_select, choice, gbtid):
 
     gpio_dirH_node = getNode("LPGBT.RWF.PIO.PIODIRH")
     gpio_outH_node = getNode("LPGBT.RWF.PIO.PIOOUTH")
@@ -21,10 +20,37 @@ def lpgbt_sub_reset(system, oh_select, gbtid_list):
     gpio_dirH_addr = gpio_dirH_node.address
     gpio_outH_addr = gpio_outH_node.address
 
-    gpio = 9
+    gpio_for_sub = 9
+    gpio_for_vtrx = 13
     boss = 1
 
-    for gbtid in gbtid_list:
+    if choice == "vtrx":
+        print("VTRX RESET\n")
+        if system == "chc":
+            config_initialize_chc(boss)
+            check_lpgbt_ready()
+        if system != "dryrun" and system != "backend":
+            check_rom_readback()
+
+        # Set GPIO as output
+        gpio_dirH_output = 0x20
+
+        if system == "backend":
+            mpoke(gpio_dirH_addr, gpio_dirH_output)
+        else:
+            writeReg(gpio_dirH_node, gpio_dirH_output, 0)
+
+        print("Set GPIO as output, register: 0x%03X, value: 0x%02X" % (gpio_dirH_addr, gpio_dirH_output))
+        sleep(0.000001)
+
+        data_enable = convert_gpio_reg(gpio_for_vtrx)
+        data_disable = 0x00
+
+        gpio_out_addr = gpio_outH_addr
+        gpio_out_node = gpio_outH_node
+
+    elif choice == "sub":
+        print("SUB RESET\n")
         if system == "backend":
             check_lpgbt_link_ready(oh_select, gbtid)
             select_ic_link(oh_select, gbtid)
@@ -46,29 +72,29 @@ def lpgbt_sub_reset(system, oh_select, gbtid_list):
         print("Set GPIO as output, register: 0x%03X, value: 0x%02X" % (gpio_dirH_addr, gpio_dirH_output))
         sleep(0.000001)
 
-        data_enable = convert_gpio_reg(gpio)
+        data_enable = convert_gpio_reg(gpio_for_sub)
         data_disable = 0x00
 
         gpio_out_addr = gpio_outH_addr
         gpio_out_node = gpio_outH_node
 
-        # Reset - 1
-        if system == "backend":
-            mpoke(gpio_out_addr, data_enable)
-        else:
-            writeReg(gpio_out_node, data_enable, 0)
-        print("Enable GPIO to reset, register: 0x%03X, value: 0x%02X" % (gpio_out_addr, data_enable))
-        sleep(0.000001)
+    # Reset - 1
+    if system == "backend":
+        mpoke(gpio_out_addr, data_enable)
+    else:
+        writeReg(gpio_out_node, data_enable, 0)
+    print("Enable GPIO to reset, register: 0x%03X, value: 0x%02X" % (gpio_out_addr, data_enable))
+    sleep(0.000001)
 
-        # Reset - 0
-        if system == "backend":
-            mpoke(gpio_out_addr, data_disable)
-        else:
-            writeReg(gpio_out_node, data_disable, 0)
-        print("Disable GPIO, register: 0x%03X, value: 0x%02X" % (gpio_out_addr, data_disable))
-        sleep(0.000001)
+    # Reset - 0
+    if system == "backend":
+        mpoke(gpio_out_addr, data_disable)
+    else:
+        writeReg(gpio_out_node, data_disable, 0)
+    print("Disable GPIO, register: 0x%03X, value: 0x%02X" % (gpio_out_addr, data_disable))
+    sleep(0.000001)
 
-        print("")
+    print("")
 
     vfat_oh_link_reset()
     sleep(0.1)
@@ -77,15 +103,15 @@ def lpgbt_sub_reset(system, oh_select, gbtid_list):
 if __name__ == "__main__":
 
     # Parsing arguments
-    parser = argparse.ArgumentParser(description="lpGBT VFAT RESET")
+    parser = argparse.ArgumentParser(description="lpGBT SUB RESET")
     parser.add_argument("-s", "--system", action="store", dest="system", help="system = chc, backend or dryrun")
     parser.add_argument("-y", "--oh_v", action="store", dest="oh_v", help="oh_v = 2")
     parser.add_argument("-o", "--ohid", action="store", dest="ohid", help="ohid = 0-1")
-    parser.add_argument("-g", "--gbtids", action="store", nargs="+", dest="gbtids", help="list of gbtids = 0-7 (only needed for backend)")
+    parser.add_argument("-c", "--choice", action="store", dest="choice", help="choice = vtrx or sub")
+    parser.add_argument("-g", "--gbtid", action="store", dest="gbtid", help="gbtid = 0-7 (only needed for backend)")
     
     args = parser.parse_args()
 
-    gbtid_list = []
     if args.system == "chc":
         print("Using Rpi CHeeseCake for sub reset")
         args.ohid = -9999
@@ -97,7 +123,6 @@ if __name__ == "__main__":
         sys.exit()
     elif args.system == "dryrun":
         print("Dry Run - not actually running")
-        gbtid_list.append(-9999)
     else:
         print(Colors.YELLOW + "Only valid options: backend, dryrun" + Colors.ENDC)
         sys.exit()
@@ -121,22 +146,34 @@ if __name__ == "__main__":
         print(Colors.YELLOW + "Only OHID 0-1 allowed" + Colors.ENDC)
         sys.exit()
 
-    if args.gbtids is None and args.system == "backend":
-        print(Colors.YELLOW + "Please select any combination of 0, 2, 4, 6" + Colors.ENDC)
-        sys.exit()        
-    elif args.gbtids is not None and args.system == "chc":
-        print(Colors.YELLOW + "Cannot select gbtids when running chc" + Colors.ENDC)
-        sys.exit()
-    elif args.gbtids is None and args.system == "chc":
-        gbtid_list.append(-9999)
-    elif args.gbtids:
-        for gbtid in args.gbtids:
-            gbtid_int = int(gbtid)
-            master_gbtids = [0, 2, 4, 6]
-            if gbtid_int not in master_gbtids:
-                print(Colors.YELLOW + "Invalid master gbtids. Only 0, 2, 4, 6 allowed" + Colors.ENDC)
+    if args.choice == "vtrx":
+        if args.gbtid is not None:
+            print(Colors.YELLOW + "Cannot choose gbtid when resetting vtrx" + Colors.ENDC)
+            sys.exit()
+        else:
+            gbtid = -9999
+    elif args.choice == "sub":
+        if args.gbtid is None:
+            if args.system == "backend":
+                print(Colors.YELLOW + "Must choose gbtid" + Colors.ENDC)
                 sys.exit()
-            gbtid_list.append(gbtid_int)
+            if args.system == "backend":
+                print(Colors.YELLOW + "Please select gbtid 0-7" + Colors.ENDC)
+                sys.exit()
+            if args.system == "dryrun":
+                gbtid = -9999
+        elif args.gbtid is not None:
+            if args.system == "chc":
+                print(Colors.YELLOW + "Cannot select gbtid when running chc" + Colors.ENDC)
+                sys.exit()
+            else:
+                gbtid = int(args.gbtid)
+                if gbtid not in [0,2,4,6]:
+                    print(Colors.YELLOW + "Invalid master gbtid. Only 0, 2, 4, 6 allowed" + Colors.ENDC)
+                    sys.exit()
+    else:
+        print(Colors.YELLOW + "Please choose either sub or vtrx" + Colors.ENDC)
+        sys.exit()
 
     # Parsing Registers XML Files
     print("Parsing xml file...")
@@ -149,7 +186,7 @@ if __name__ == "__main__":
 
     # Running Phase Scan
     try:
-        lpgbt_sub_reset(args.system, int(args.ohid), gbtid_list)
+        lpgbt_sub_reset(args.system, int(args.ohid), args.choice, gbtid)
     except KeyboardInterrupt:
         print(Colors.RED + "Keyboard Interrupt encountered" + Colors.ENDC)
         rw_terminate()
